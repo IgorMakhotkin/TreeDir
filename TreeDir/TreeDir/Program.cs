76670cb6b6c2ba172bbf2b4c48d6a982f3e0a908
Igor <igor.makhotkin@gmail.com>
2022-04-12 17:03:11 +0300

    create main func

--- TreeDir/TreeDir/Program.cs
+++ TreeDir/TreeDir/Program.cs
@@ -4,18 +4,68 @@ namespace TreeDir
 {
     static class Program
     {
+        static void ShowParametrs()
+        {
+            Console.WriteLine("Usage: FsTree -p [dir] [-h] [-o] [dirOutput] [-q]");
+            Console.WriteLine(" -q (--quite) - признак вывода сообщений в стандартный поток вывода (если указана, то не выводить лог в консоль. Только в файл)");
+            Console.WriteLine(" -p (--path) - путь к папке для обхода (по-умолчанию текущая папка вызова программы)");
+            Console.WriteLine(" -o (--output) - путь к тестовому файлу, куда записать результаты выполнения расчёта (по-умолчанию файл sizes-YYYY-MM-DD.txt в текущей папке вызова программы)");
+            Console.WriteLine(" -h (--humanread) - признак формирования размеров файлов в человекочитаемой форме (размеры до 1Кб указывать в байтах, размеры до 1Мб в килобайтах с 2 знаками после запятой, размеры до 1Гб в мегабайтах с 2 знаками после запятой, размеры до 1Тб - в Гб с 2 знаками после запятой)");
+        }
         static void Main(string[] args)
         {
             string startDir = null;
-            for (int i = 0;i < args.Length;i++)
+            bool printInFile = false;
+            bool returnHumanRead = false;
+            string output = null;
+
+            for (int i = 0; i < args.Length; i++)
             {
                 string arg = args[i];
-                if(arg == "-h")
+                if (arg == "-q" || arg == "--quite")
                 {
-                 RecTree d = new RecTree(startDir);
-                    d.Print(args[i+1]);
+                    printInFile = true;
+                }
+                else if (arg == "-h" || arg == "--humanread")
+                {
+                    returnHumanRead = true;
+                }
+                else if (arg == "-o" || arg == "--output")
+                {
+                    output = args[i + 1];
+                    i++;
+                    if (output == " ")
+                    {
+                        output = ".";
+                    }
+                }
+                else if (arg == "-p" || arg == "--path")
+                {
+                    startDir = args[i + 1];
+                    i++;
+                    if (startDir == " ")
+                    {
+                        startDir = ".";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный параметр {0}", arg);
+                    ShowParametrs();
+                    return;
                 }
             }
+            if (String.IsNullOrEmpty(startDir))
+            {
+                startDir = ".";
+            }
+
+            new RecTree(startDir)
+            {
+                PrintInFile = printInFile,
+                ReturnHumanRead = returnHumanRead
+
+            }.Print();
         }
     }
 }
