df12123bc1afe4895fd3cbd22cfcd07e51e5eae2
Igor <igor.makhotkin>
2022-04-13 11:01:46 +0300

    Add input in file

--- TreeDir/TreeDir/RecTree.cs
+++ TreeDir/TreeDir/RecTree.cs
@@ -11,15 +11,24 @@ namespace TreeDir
         public RecTree(string startDir)
         {
             StartDir = startDir;
+            
         }
 
         public string StartDir { get; set; }
-        public bool PrintInFile { get; set; } = true;
+        public string Output = "C:\\Output.txt";
+        public bool PrintInFile { get; set; } = false;
         public bool ReturnHumanRead { get; set; } = false;
         public void Print()
         {
-            WriteColored(StartDir, DirColor);
-            WriteLine();
+            if(PrintInFile)
+            {
+                printDirInFile(Output, StartDir);
+            }
+            else
+            {
+                WriteColored(StartDir, DirColor);
+                WriteLine();
+            }
             PrintTree(StartDir);
         }
         public Action<string> Write { get; set; } = Console.Write;
@@ -98,6 +107,38 @@ namespace TreeDir
         {
             WriteColored(fsItem.Name, GetColor(fsItem));
         }
+        private void printInFile(string Output,FileSystemInfo fsItem,FileInfo file)
+        {
+            StreamWriter result = new StreamWriter(Output, true);
+            result.Write(fsItem.Name);
+            result.Write(" ");
+            result.Write(ReturnSize(file.Length));
+            result.Close();
+        }
+        private void printInFile(string Output, FileSystemInfo fsItem)
+        {
+            StreamWriter result = new StreamWriter(Output, true);
+            result.Write(fsItem.Name);
+            result.Close();
+        }
+        private void printInFile(string Output, string prefix)
+        {
+            StreamWriter result = new StreamWriter(Output, true);
+            result.Write(prefix);
+            result.Close();
+        }
+        private void printInFile(string Output)
+        {
+            StreamWriter result = new StreamWriter(Output, true);
+            result.WriteLine();
+            result.Close();
+        }
+        private void printDirInFile(string Output,string startdir)
+        {
+            StreamWriter result = new StreamWriter(Output, true);
+            result.WriteLine(startdir);
+            result.Close();
+        }
         private void PrintTree(string startDir, string prefix = "")
         {
             var di = new DirectoryInfo(startDir);
@@ -108,7 +149,13 @@ namespace TreeDir
             // Проходим список
             foreach (var fsItem in fsItems.Take(fsItems.Count - 1))
             {
+                if(PrintInFile)
+                {
+                    printInFile(Output, prefix + "├── ");
+                }
+                else
                 Write(prefix + "├── ");
+                
                 // если не является директорией
                 if (!fsItem.IsDirectory())
                 {
@@ -118,16 +165,32 @@ namespace TreeDir
                     foreach (FileInfo file in files)
                     {
                         //Печатаем имя файла и его размер
-                        WriteName(fsItem, ReturnSize(file.Length));
-                        WriteLine();
+                        if(PrintInFile)
+                        {
+                            printInFile(Output, fsItem,file);
+                            printInFile(Output);
+                        }
+                        else
+                        {
+                            WriteName(fsItem, ReturnSize(file.Length));
+                            WriteLine();
+                        }
                     }
                 }
                 //Если является директорией 
                 if (fsItem.IsDirectory())
                 {
                     //Печатаем имя папки
-                    WriteName(fsItem);
-                    WriteLine();
+                    if(PrintInFile)
+                    {
+                        printInFile(Output, fsItem);
+                        printInFile(Output);
+                    }
+                    else
+                    {
+                        WriteName(fsItem);
+                        WriteLine();
+                    }
                     //Рекурсивно вызываем функцию передавая имя текущей папки
                     PrintTree(fsItem.FullName, prefix + "│   ");
                 }
@@ -136,18 +199,41 @@ namespace TreeDir
             var lastFsItem = fsItems.LastOrDefault();
             if (lastFsItem != null)
             {
-                Write(prefix + "└── ");
+                if(PrintInFile)
+                {
+                    printInFile(Output, prefix + "└── ");
+                }
+                else
+                {
+                    Write(prefix + "└── ");
+                }
                 FileInfo[] files = null;
                 files = di.GetFiles(lastFsItem.Name);
                 foreach (FileInfo file in files)
                 {
-                    WriteName(lastFsItem, ReturnSize(file.Length));
-                    WriteLine();
+                    if(PrintInFile)
+                    {
+                        printInFile(Output, lastFsItem, file);
+                        printInFile(Output);
+                    }
+                    else
+                    {
+                        WriteName(lastFsItem, ReturnSize(file.Length));
+                        WriteLine();
+                    }
                 }
                 if (lastFsItem.IsDirectory())
                 {
-                    WriteName(lastFsItem);
-                    WriteLine();
+                    if(PrintInFile)
+                    {
+                        printInFile(Output, lastFsItem);
+                        printInFile(Output);
+                    }
+                    else
+                    {
+                        WriteName(lastFsItem);
+                        WriteLine();
+                    }
                     PrintTree(lastFsItem.FullName, prefix + "    ");
                 }
             }
